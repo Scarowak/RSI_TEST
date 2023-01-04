954ca707a8a278036613a36d8213fff903410cb7
Krzysztof Iwanek <krzysztofiwanek01@wp.pl>
2023-01-04 20:00:22 +0100

    Ustalenie zmiennych

--- RSI_TEST.py
+++ RSI_TEST.py
@@ -5,3 +5,26 @@ Created on Wed Jan  4 19:49:18 2023
 @author: Krzychu
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas_datareader import data as pdr
+import datetime as dt
+import yfinance as yf
+from matplotlib.pyplot import figure
+
+pd.options.mode.chained_assignment = None  # default='warn' 
+plt.style.use('default')   #styl wykresów
+
+
+#1 Zmienne 
+start = dt.datetime(2018,2,1)   #Początek badanego zakresu danych
+end = dt.datetime(2023,9,6)   #Koniec badanego zakresu danych
+rsi_lenght = 14 #Argument rsi (dla ilu okresów badane)
+
+#2 Wczytanie danych z yahoo
+company = ["^GSPC"]  #ticker z https://finance.yahoo.com/
+
+yf.pdr_override()
+df=pdr.get_data_yahoo(company, start, end)
+
+
